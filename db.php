45ade778dc39229d75a96425e51446fcaeab0119
berktekin <58903526+berktekin@users.noreply.github.com>
2019-12-15 15:39:54 +0300

    Update db.php

--- db.php
+++ db.php
@@ -7,6 +7,35 @@
 	*/
 	// define('DATABASE', 'mssql'); 
 
+	<?php
+$host = 'attendance1-mysqldbserver.mysql.database.azure.com';
+$username = 'mysqldbuser@attendance1-mysqldbserver';
+$password = 'admin';
+$db_name = 'DATABASE';
+
+//Establishes the connection
+$conn = mysqli_init();
+mysqli_real_connect($conn, $host, $username, $password, $db_name, 3306);
+if (mysqli_connect_errno($conn)) {
+die('Failed to connect to MySQL: '.mysqli_connect_error());
+}
+
+// Run the create table query
+if (mysqli_query($conn, '
+CREATE TABLE Products (
+`Id` INT NOT NULL AUTO_INCREMENT ,
+`ProductName` VARCHAR(200) NOT NULL ,
+`Color` VARCHAR(50) NOT NULL ,
+`Price` DOUBLE NOT NULL ,
+PRIMARY KEY (`Id`)
+);
+')) {
+printf("Table created\n");
+}
+
+//Close the connection
+mysqli_close($conn);
+
 
 
 	if(!defined('DATABASE')){
