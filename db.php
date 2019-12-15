f560431878b5e8f20086742ee0f5db32104cd568
berktekin <58903526+berktekin@users.noreply.github.com>
2019-12-15 15:24:34 +0300

    Update db.php

--- db.php
+++ db.php
@@ -8,7 +8,13 @@
 	// define('DATABASE', 'mssql'); 
 
 
-
+	try{
+$db=new PDO("mysql:host=attendance1-mysqldbserver.mysql.database.azure.com;dbname=mysqldatabase8658;charset=utf8","mysqldbuser@attendance1-mysqldbserver","Brk1997brk");
+$db->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION );  
+}
+catch(PDOException $e){
+    echo $e->getMessage();
+}
 
 	if(!defined('DATABASE')){
 		if(function_exists('mysqli_connect')){
